ee851272a4f212de5531019cbfe86c2cbc9db1de
onojastanley <40136094+onojastanley@users.noreply.github.com>
2021-12-22 23:03:11 +0100

    Add files via upload

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+
+    public function Logout(){
+    	Auth::logout();
+    	return Redirect()->route('login');
+
+    }
+    //
+}
