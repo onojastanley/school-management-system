f16c6b6f6b4a8f01104d72447138cbc676e3d991
onojastanley <40136094+onojastanley@users.noreply.github.com>
2021-12-22 23:49:12 +0100

    Add files via upload

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -1,3 +1,8 @@
+@php
+$prefix = Request::route()->getPrefix();
+$route = Route::current()->getName();
+
+@endphp
 <aside class="main-sidebar">
     <!-- sidebar-->
     <section class="sidebar">
@@ -14,18 +19,19 @@
             </div>
         </div>
 
-      <!-- sidebar menu-->
-      <ul class="sidebar-menu" data-widget="tree">
+        <!-- sidebar menu-->
+        <ul class="sidebar-menu" data-widget="tree">
 
-        <li>
-          <a href="index.html">
-            <i data-feather="pie-chart"></i>
-            <span>Dashboard</span>
-          </a>
-        </li>
+            <li class="{{ ($route == 'dashboard')?'active':'' }}" >
+              <a href="{{ route('dashboard') }}">
+                <i data-feather="pie-chart"></i>
+                <span>Dashboard</span>
+              </a>
+            </li>
 
-        <li class="treeview">
-            <a href="#">
+        @if(Auth::user()->role == 'Admin')
+            <li class="treeview {{ ($prefix == '/users')?'active':'' }} " >
+              <a href="#">
                 <i data-feather="message-circle"></i>
                 <span>Manage User</span>
                 <span class="pull-right-container">
@@ -37,18 +43,19 @@
                 <li><a href="{{ route('users.add') }}"><i class="ti-more"></i>Add User</a></li>
               </ul>
         </li>
+        @endif
 
-        <li class="treeview">
+        <li class="treeview {{ ($prefix == '/profile')?'active':'' }}">
           <a href="#">
-            <i data-feather="mail"></i> <span>Mailbox</span>
+            <i data-feather="grid"></i> <span>Manage Profile</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-right pull-right"></i>
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="mailbox_inbox.html"><i class="ti-more"></i>Inbox</a></li>
-            <li><a href="mailbox_compose.html"><i class="ti-more"></i>Compose</a></li>
-            <li><a href="mailbox_read_mail.html"><i class="ti-more"></i>Read</a></li>
+        <li><a href="{{ route('profile.view') }}"><i class="ti-more"></i>Your Profile</a></li>
+        <li><a href="{{ route('password.view') }}"><i class="ti-more"></i>Change Password</a></li>
+
           </ul>
         </li>
 
